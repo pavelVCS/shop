5feda7209ca22d5c57331e4f232d4a18fa777657
pavel <pavel@setupad.com>
2024-02-06 23:01:21 +0200

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// components
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navbar.scss';
+
+function Navbar() {
+  return (
+    <nav className="nav-container">
+      <h1>My Shop</h1>
+      <ul>
+        <li>All items</li>
+        <li>Favorite</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
