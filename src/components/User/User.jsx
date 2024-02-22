db11498a4985ce10d2cf24283650dda306fa6fd3
pavel <--local>
2024-02-22 19:38:02 +0200

    fix user endpoint

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -30,7 +30,7 @@ function User() {
       setLoading(true);
       try {
         const response = await fetch(
-          'https://jsonplaceholder.typicode.com/users/111'
+          'https://jsonplaceholder.typicode.com/users/1'
         );
 
         if (!response.ok) throw new Error('Something went wrong');
