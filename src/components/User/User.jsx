e715016ef5f06f079109adee5a802fd37e86e46c
pavel <--local>
2024-02-22 19:46:14 +0200

    user changed

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -30,9 +30,9 @@ function User() {
       setLoading(true);
       try {
         const response = await fetch(
-          'https://jsonplaceholder.typicode.com/users/1'
+          'https://jsonplaceholder.typicode.com/users/2'
         );
-
+        console.log(response);
         if (!response.ok) throw new Error('Something went wrong');
 
         const data = await response.json();
